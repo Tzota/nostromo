1626ed55e1c7d42e6486fe7a45e2455f5fb12849
Daniyal Guliev <tzota@mail.ru>
2020-05-28 22:33:43 +0300

    change mysql docker image to arm7-compatible

--- scripts/prepare/10.mysql.sh
+++ scripts/prepare/10.mysql.sh
@@ -4,4 +4,7 @@ set -euo pipefail
 docker create \
     -v nostromo-mysql-volume:/var/lib/mysql \
     --name nostromo-mysql \
-    -e MYSQL_ROOT_PASSWORD=root mysql
+    -e MYSQL_ROOT_PASSWORD=root \
+    -p 3306:3306 \
+    # maybe clover/mysqld and clover/mysql are good too
+    hypriot/rpi-mysql
