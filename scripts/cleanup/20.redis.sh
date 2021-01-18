8a1e136aeb82383ecb694d7712171d41c2ed2b6c
Daniyal Guliev <tzota@mail.ru>
2021-01-19 00:25:00 +0300

    prepare redis to be started on ubuntu as well

--- scripts/cleanup/20.redis.sh
+++ scripts/cleanup/20.redis.sh
@@ -1,4 +1,32 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
+if [ $UID -ne 0 ]
+then
+  echo "You are NOT root!"
+  exit 1
+fi
+
+ARCH=$(uname -m)
+if [ $ARCH = "armv7l" ]
+then
+    #maybe hypriot/redis is good too
+    REDIS_IMAGE=arm32v7/redis
+elif [ $ARCH = "x86_64" ]
+then
+    REDIS_IMAGE=redis
+else
+    echo  "UNKNOWN ARCHITECTURE"
+    exit 1
+fi
+
+#######################
+
+
 docker container rm nostromo-redis
+
+docker image rm $REDIS_IMAGE
+
+rm -rf /opt/nostromo-redis/
+
+# clean up vm.overcommit_memory = 1 from /etc/sysctl.conf
--- scripts/prepare/20.redis.sh
+++ scripts/prepare/20.redis.sh
@@ -1,14 +1,41 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
+if [ $UID -ne 0 ]
+then
+  echo "You are NOT root!"
+  exit 1
+fi
+
+ARCH=$(uname -m)
+if [ $ARCH = "armv7l" ]
+then
+    #maybe hypriot/redis is good too
+    REDIS_IMAGE=arm32v7/redis
+elif [ $ARCH = "x86_64" ]
+then
+    REDIS_IMAGE=redis
+else
+    echo  "UNKNOWN ARCHITECTURE"
+    exit 1
+fi
+
+#######################
+
 grep -qx 'vm.overcommit_memory = 1' /etc/sysctl.conf || printf "\n\n# tzota - prepare for redis\nvm.overcommit_memory = 1\n\n" >> /etc/sysctl.conf
 
-#maybe hypriot/redis is good too
 docker create \
     --name nostromo-redis \
     -p 127.0.0.1:6379:6379 \
     --restart unless-stopped \
-    arm32v7/redis \
+    $REDIS_IMAGE \
     --appendonly yes \
     --maxmemory 512mb \
     --tcp-backlog 128
+
+mkdir -p /opt/nostromo-redis/{etc,data}
+mkdir /opt/nostromo-redis/data/{bases,log}
+
+REDIS_VERSION=$(docker image inspect $REDIS_IMAGE | grep -e REDIS_VERSION | head -1 | sed -E 's/\s*"REDIS_VERSION=([0-9\.]*)",/\1/')
+echo $REDIS_VERSION
+curl -s -o /opt/nostromo-redis/etc/redis.conf https://raw.githubusercontent.com/antirez/redis/$REDIS_VERSION/redis.conf
